f0efb3b480f8a65960ea97503dcc8d065f059e46
Konstantin Grachev <kgrachev@gmail.com>
2021-03-23 16:47:52 +0000

    feat(server): add search endpoint to API

--- server/router.js
+++ server/router.js
@@ -15,7 +15,7 @@ router.get('/interesting/:size', async (req, res) => {
   const { size } = req.params;
 
   const extras = `&extras=url_${size}%2C+description%2C+tags%2C+owner_name`
-  const url = `${baseUrl}/rest/?api_key=${process.env.API_KEY + querySettings}&method=flickr.interestingness.getList&per_page=10${extras}`;
+  const url = `${baseUrl}/rest/?api_key=${process.env.API_KEY + querySettings}&method=flickr.interestingness.getList&per_page=1${extras}`;
 
   const fetchRes = await fetch(url);
   const jsonRes = await fetchRes.json();
@@ -31,5 +31,30 @@ router.get('/getUserUrl/:id', async (req, res) => {
   const jsonRes = await fetchRes.json();
   res.send(jsonRes);
 });
+
+
+router.get('/search/:query/:pageNum', async (req, res) => {
+  const { query, pageNum } = req.params;
+  const url = baseUrl + '/rest/?' + new URLSearchParams({
+    text: query,
+    page: pageNum,
+    extras: ['url_s', 'description', 'tags', 'owner_name'],
+    api_key: 'fced83294eb6fb247c8febeb9c10770d',
+    method: 'flickr.photos.search',
+    format: 'json',
+    nojsoncallback: 1,
+    per_page: 10
+  }).toString();
+
+  console.log('SEARCHING :::    ', query)
+
+
+  const fetchRes = await fetch(url);
+
+  const jsonRes = await fetchRes.json();
+
+  // console.log('SEARCH RESULT JSON:::  ', jsonRes);
+  res.send(jsonRes);
+})
 
 module.exports = router;
