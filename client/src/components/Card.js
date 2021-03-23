42ad882a7fd7947f75d37daf2ff78e31247a90ee
Konstantin Grachev <kgrachev@gmail.com>
2021-03-23 01:59:25 +0000

    feat(client): create card component

---
+++ client/src/components/Card.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import '../css/Card.css';
+
+
+
+
+export default function Card ({ title, photoUrl, author, authorUrl, description, tags }) {
+  console.log(authorUrl);
+
+  useEffect(() => {
+
+  }, []);
+
+  function renderTags () {
+    return tags.split(' ').map(tag => {
+      return <li ><div className="tag-text">{ tag}</div><button >+</button></li>
+    })
+  }
+
+  return (
+    <div className="card-container">
+      <figure>
+        <img src={photoUrl} alt={title} />
+        <figcaption>
+          <div className="details">
+            <a href={photoUrl} className="title">{title}</a>
+            <div className="author">
+              <i>by <a href={authorUrl} >{author}</a></i>
+            </div>
+          </div>
+          <p className="description">
+            {/* {description} */}
+          </p>
+          <ul className="tags-list">{renderTags()}
+            <div className="shadow-box"></div>
+          </ul>
+        </figcaption>
+      </figure>
+    </div>
+  )
+}
