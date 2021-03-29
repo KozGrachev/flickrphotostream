c07180d615fe91206f5f096f63f456b78cec458b
Konstantin Grachev <kgrachev@gmail.com>
2021-03-29 11:03:17 +0100

    feat: add scripts for deployment

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const app = express();
 require('dotenv').config({ path: __dirname + '/.env' });
 app.use(require('cors')());
 
-app.use(express.json());
 app.use(require('./router'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.listen(3010, () => {
   console.log("Listening on port 3010");
