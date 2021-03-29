de8554495c2e3664384f448e8f2d09cd073f93bc
Konstantin Grachev <kgrachev@gmail.com>
2021-03-29 13:13:49 +0100

    feat: add scripts for deployment

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const PORT = process.env.PORT || 3010;
 
 const app = express();
-// const baseUrl = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json';
 require('dotenv').config({ path: __dirname + '/.env' });
 app.use(require('cors')());
 
-app.use(express.json());
 app.use(require('./router'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
-app.listen(3010, () => {
+app.listen(PORT, () => {
   console.log("Listening on port 3010");
 });
